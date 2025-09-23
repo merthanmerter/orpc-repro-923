09a77b87097905ed7919de2e9926fe07f5ffdc62
Merthan Merter <merthanm@gmail.com>
2025-09-24 02:42:26 +0300

    Testing

--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,10 @@
 import { createORPCClient } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
 import type { RouterClient } from "@orpc/server";
-import { createTanstackQueryUtils } from "@orpc/tanstack-query";
+import {
+	createTanstackQueryUtils,
+	type ProcedureUtils,
+} from "@orpc/tanstack-query";
 import type { router } from "@server/index";
 import { useQuery } from "@tanstack/react-query";
 
@@ -13,17 +16,46 @@ export const orpc: RouterClient<typeof router> = createORPCClient(link);
 export const orpcClient = createTanstackQueryUtils(orpc);
 
 export default async function Client() {
-	const { data: dataFromHook } = useQuery(
-		orpcClient.planet.find.queryOptions({
+	const procedure = orpcClient.planet.find;
+
+	const query1 = useQuery(
+		procedure.queryOptions({
 			input: { id: "1" },
 		}),
 	);
-	const directCall = await orpcClient.planet.find.call({
-		id: "1",
+
+	const query2 = useQuery<Q<typeof procedure.call>>(
+		procedure.queryOptions({
+			input: { id: "1" },
+		}),
+	);
+
+	const orpcQuery = useOrpcQuery(orpcClient.planet.find, {
+		input: { id: "1" },
 	});
 
-	dataFromHook?.id; // unknown
-	directCall?.id; // ok
+	if (query1.isSuccess) {
+		// @ts-expect-error
+		return <div>Success: {query1.data.id}</div>;
+	}
+
+	if (query2.isSuccess) {
+		return <div>Success: {query2.data.id}</div>;
+	}
+
+	if (orpcQuery.isSuccess) {
+		return <div>Success: {orpcQuery.data.id}</div>;
+	}
 
 	return <div>Client</div>;
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: Any
+type Q<T extends (...args: any[]) => any> = Awaited<ReturnType<T>>;
+// biome-ignore lint/suspicious/noExplicitAny: Any
+function useOrpcQuery<T extends ProcedureUtils<any, any, any, any>>(
+	procedure: T,
+	options: Parameters<T["queryOptions"]>[0],
+) {
+	return useQuery<Q<T["call"]>>(procedure.queryOptions(options));
 }
